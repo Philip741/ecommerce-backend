33b02f35469ef684cdfaff4f546ff1be5390eb14
Philip Browning <philip741@gmail.com>
2021-11-03 15:19:41 -0400

    Updated product tag model

--- models/ProductTag.js
+++ models/ProductTag.js
@@ -6,6 +6,20 @@ class ProductTag extends Model {}
 
 ProductTag.init(
   {
+    id: {
+          type: DataTypes.INTEGER,
+          allowNull: false,
+          primaryKey: true,
+          autoIncrement: true
+    },
+    product_id: {
+          type: DataTypes.INTEGER,
+          allowNull: false,
+    },
+    tag_id: {
+          type: DataTypes.INTEGER,
+          allowNull: false,
+    },
     // define columns
   },
   {
